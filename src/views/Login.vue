47a618934f5a972a2951fe131bfe57fdd9307877
s-koyanagi <kysh19931216@gmail.com>
2020-01-23 19:43:34 +0900

    minor fix

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,52 +1,46 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col md="3">
-            <v-card>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    id="userid"
-                    label="ID"
-                    type="text"
-                    placeholder=" "
-                    dense="true"
-                    outlined
-                    class="login-form"
-                  />
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    type="password"
-                    placeholder=" "
-                    dense="true"
-                    outlined
-                    class="login-form"
-                    hide-details="true"
-                  />
-                  <a style="text-align:center;" href="#"
-                    >パスワード忘れた方はコチラ</a
-                  >
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn
-                  block="true"
-                  color="primary"
-                  full-width="true"
-                  class="login-button"
-                  large
-                  rounded
-                  >ログイン</v-btn
-                >
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-content>
+  <v-app id="login">
+    <v-container>
+      <v-card>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              id="userid"
+              label="ID"
+              type="text"
+              placeholder=" "
+              dense="true"
+              outlined
+              class="login-form"
+            />
+            <v-text-field
+              id="password"
+              label="Password"
+              type="password"
+              placeholder=" "
+              dense="true"
+              outlined
+              class="login-form"
+              hide-details="true"
+            />
+            <a style="text-align:center;" href="#"
+              >パスワードを忘れた方はコチラ</a
+            >
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            block="true"
+            color="primary"
+            full-width="true"
+            class="login-button"
+            large
+            rounded
+            >ログイン</v-btn
+          >
+        </v-card-actions>
+      </v-card>
+    </v-container>
   </v-app>
 </template>
 <script>
