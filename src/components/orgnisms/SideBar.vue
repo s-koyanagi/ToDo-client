b94e47718390d4706161bf221282908fa536b571
s-koyanagi <kysh19931216@gmail.com>
2021-01-11 01:48:18 +0900

    add sidebar

---
+++ src/components/orgnisms/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-navigation-drawer app>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class="title"> Application </v-list-item-title>
+        <v-list-item-subtitle> subtext </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense nav>
+      <!-- <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item> -->
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator'
+@Component
+export default class SideBar extends Vue {}
+</script>
