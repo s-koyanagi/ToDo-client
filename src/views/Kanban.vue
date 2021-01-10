b688beb2900169e69cb428cab851840c6780bb08
s-koyanagi <kysh19931216@gmail.com>
2021-01-11 01:48:34 +0900

    fix layout

--- src/views/Kanban.vue
+++ src/views/Kanban.vue
@@ -1,72 +1,55 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <v-sheet>
-          <p style="margin-bottom: 0px;" class="headline pt-3 pl-3">
-            Task List
-          </p>
-          <v-container>
-            <v-row dense>
-              <v-col cols="4">
-                <v-sheet color="red lighten-4" height="495" elevation="2">
-                  <p style="margin-bottom: 12px;" class="title pt-3 pl-3">
-                    Waiting
-                  </p>
-                  <v-container>
-                    <v-row dense v-for="task in tasks" :key="task.id">
-                      <v-col>
-                        <v-card>
-                          <v-card-title class="title pt-2 pb-2">{{
-                            task.title
-                          }}</v-card-title>
-                          <v-divider></v-divider>
-                          <v-card-text>{{ task.detail }}</v-card-text>
-                        </v-card>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-sheet>
-              </v-col>
-              <v-col cols="4">
-                <v-sheet color="blue lighten-4" height="495" elevation="2">
-                  <p style="margin-bottom: 12px;" class="title pt-3 pl-3">
-                    Working
-                  </p>
-                </v-sheet>
-              </v-col>
-              <v-col cols="4">
-                <v-sheet color="teal lighten-4" height="495" elevation="2">
-                  <p style="margin-bottom: 12px;" class="title pt-3 pl-3">
-                    Completed
-                  </p>
-                </v-sheet>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-row no-gutters>
+    <v-col cols="2">
+      <v-card class="grey lighten-3" min-height="800" outlined tile>
+        <p style="margin-bottom: 0px" class="headline pt-3 pl-3">Filter</p>
+        <!-- <v-container>
+          <v-row dense>
+            <v-col></v-col>
+          </v-row>
+        </v-container> -->
+      </v-card>
+    </v-col>
+
+    <v-col cols="10">
+      <v-card min-height="800" outlined tile>
+        <v-app-bar class="ma-5" color="white" flat elevate-on-scroll>
+          <v-btn x-large icon><v-icon>mdi-arrow-left</v-icon></v-btn>
+          <v-toolbar-title class="mr-3">2020/1/1</v-toolbar-title>
+          <v-btn x-large icon><v-icon>mdi-arrow-right</v-icon></v-btn>
+          <v-spacer></v-spacer>
+          <v-btn icon><v-icon>mdi-dots-vertical</v-icon></v-btn>
+        </v-app-bar>
+        <v-card-text class="ma-5">
+          <v-simple-table>
+            <thead>
+              <tr>
+                <th>subject</th>
+                <th>status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>友達とランチ</td>
+                <td>完了</td>
+              </tr>
+              <tr>
+                <td>評価書提出</td>
+                <td>進行中</td>
+              </tr>
+            </tbody>
+          </v-simple-table>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import { Component, Vue, Watch } from 'vue-property-decorator'
-import MenuBar from '../components/orgnisms/MenuBar'
 
 @Component({
-  components: {
-    MenuBar
-  }
+  components: {}
 })
-export default class Kanban extends Vue {
-  tasks = [
-    {
-      id: '1',
-      title: 'テスト1',
-      detail: 'テスト1テスト1テスト1テスト1テスト1'
-    },
-    { id: '2', title: 'テスト2', detail: 'テスト2テスト2テスト2テスト2テスト2' }
-  ]
-}
+export default class Kanban extends Vue {}
 </script>
