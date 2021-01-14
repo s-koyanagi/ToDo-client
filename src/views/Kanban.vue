b92df5a13d216edc2e881b37fce3d28ed6d5f2d4
s-koyanagi <kysh19931216@gmail.com>
2021-01-14 23:53:58 +0900

    DataTable support

--- src/views/Kanban.vue
+++ src/views/Kanban.vue
@@ -3,11 +3,6 @@
     <v-col cols="2">
       <v-card class="grey lighten-3" min-height="800" outlined tile>
         <p style="margin-bottom: 0px" class="headline pt-3 pl-3">Filter</p>
-        <!-- <v-container>
-          <v-row dense>
-            <v-col></v-col>
-          </v-row>
-        </v-container> -->
       </v-card>
     </v-col>
 
@@ -21,35 +16,31 @@
           <v-btn icon><v-icon>mdi-dots-vertical</v-icon></v-btn>
         </v-app-bar>
         <v-card-text>
-          <v-simple-table class="ma-5">
-            <thead>
-              <tr>
-                <th>type</th>
-                <th>subject</th>
-                <th>status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="task in tableData" :key="task.id">
-                <td>{{ task.type }}</td>
-                <td>{{ task.subject }}</td>
-                <td>{{ task.status }}</td>
-              </tr>
-            </tbody>
-          </v-simple-table>
+          <data-table class="ma-5" :data="tableData" :header="head">
+          </data-table>
         </v-card-text>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator'
+import DataTable from '../components/organisms/DataTable.vue'
 
 @Component({
-  components: {}
+  components: {
+    DataTable
+  }
 })
 export default class Kanban extends Vue {
+  search: String = ''
+  head = [
+    { text: 'Type', align: 'start', value: 'type' },
+    { text: 'Subject', align: 'start', value: 'subject' },
+    { text: 'Status', align: 'start', value: 'status' }
+  ]
+
   tableData = [
     { id: 1, type: 'プライベート', subject: '友達とランチ', status: '未着手' },
     { id: 2, type: '仕事', subject: 'テスト仕様書作成', status: '未着手' },
