32a9f5827991e744ff1ade865a81e816258c685f
s-koyanagi <kysh19931216@gmail.com>
2021-01-14 23:51:25 +0900

    add DataTable

---
+++ src/components/organisms/DataTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="header"
+      :items="data"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class DataTable extends Vue {
+  @Prop()
+  private header!: object[]
+  @Prop()
+  private data!: object[]
+  search: string = ''
+}
+</script>
