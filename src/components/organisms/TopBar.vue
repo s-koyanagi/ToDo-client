974e0543ae30fe5e101e581d6d4ea6f4a19480a6
s-koyanagi <kysh19931216@gmail.com>
2021-02-01 23:12:27 +0900

    add logout function

--- src/components/organisms/TopBar.vue
+++ src/components/organisms/TopBar.vue
@@ -11,15 +11,20 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn outlined>
+    <v-btn outlined @click="logout">
       ログアウト
     </v-btn>
   </v-app-bar>
 </template>
 
 <script lang="ts">
+  import { loginUser } from '@/store/modules/loginUser';
   import { Component, Vue } from 'vue-property-decorator';
 
   @Component
-  export default class TopBar extends Vue {}
+  export default class TopBar extends Vue {
+    async logout() {
+      await loginUser.logout();
+    }
+  }
 </script>
