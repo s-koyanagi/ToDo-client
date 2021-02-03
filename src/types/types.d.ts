d8e6195c7d882499d29da630dcac63ca3e5e9bf5
s-koyanagi <kysh19931216@gmail.com>
2021-02-03 23:14:30 +0900

    minor fix

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,19 +1,19 @@
 export interface ProjectData {
-  projectId: number;
-  projectName: string;
-  color: string;
+  projectId?: number;
+  projectName?: string;
+  color?: string;
 }
 
 export interface TaskData {
-  taskid: number;
-  projectId: number;
-  subject: string;
-  statusId: number;
-  deadLine: string;
+  taskid?: number;
+  projectId?: number;
+  subject?: string;
+  statusId?: number;
+  deadLine?: string;
 }
 
 export interface StatusData {
-  statusId: number;
-  statusName: string;
-  color: string;
+  statusId?: number;
+  statusName?: string;
+  color?: string;
 }
