42a749e990ab7ead38572ea49d40d5c41b677ed2
s-koyanagi <kysh19931216@gmail.com>
2021-02-04 23:38:49 +0900

    add action getter

--- src/store/modules/task.ts
+++ src/store/modules/task.ts
@@ -1,24 +1,50 @@
-import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
+import {
+  Module,
+  VuexModule,
+  Mutation,
+  Action,
+  getModule,
+} from 'vuex-module-decorators';
 
 import store from '../index';
 import axios, { AxiosError, AxiosResponse } from 'axios';
-import router from '@/router';
 import { TaskData } from '@/types/types';
 
 @Module({
   dynamic: true,
   store,
-  name: 'task',
+  name: 'Task',
   namespaced: true,
 })
-class task extends VuexModule {
-  private taskData: TaskData[] = [];
+class Task extends VuexModule {
+  private taskList: TaskData[] = [];
 
   @Mutation
-  private SET_TASK_DATA(task: TaskData[]) {
-    this.taskData = task;
+  private SET_TASK_LIST(task: TaskData[]) {
+    this.taskList = task;
   }
 
   @Action({ rawError: true })
-  async getTaskData(projectId: number) {}
+  async getTaskList(projectId?: number) {
+    if (projectId === undefined) {
+      await axios
+        .post('/task/get-list')
+        .then((res: AxiosResponse) => {
+          this.SET_TASK_LIST(res.data);
+        })
+        .catch((err: AxiosError) => {});
+    } else {
+      await axios
+        .post('/task/get-list', projectId)
+        .then((res: AxiosResponse) => {
+          this.SET_TASK_LIST(res.data);
+        })
+        .catch((err: AxiosError) => {});
+    }
+  }
+
+  get GET_TASK_LIST(): TaskData[] {
+    return this.taskList;
+  }
 }
+export const taskStore = getModule(Task);
